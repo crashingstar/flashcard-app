155a9d6b89eca3dfb5416c3e761dd1ed6e9d35af
zhi yao <teezhiyao@gmail.com>
2022-04-03 06:45:22 +0800

    Add create_user api

--- backend/UserApi.py
+++ backend/UserApi.py
@@ -1,11 +1,24 @@
 import time
-from flask import Blueprint
+from flask import Blueprint, request
+from db_connection import mysql
+import MySQLdb
 
-user_api = Blueprint('user_api',__name__)
+user_api = Blueprint('user_api', __name__)
 
-# @app.route('/user/create')
-# def create_user():
-#     cursor = mysql.connection.cursor()
-#     cursor.execute(''' INSERT INTO user VALUES(1,user1,password,admin,20-3-2022,teezhiyao@gmail) ''')
-#     mysql.connection.commit()
-#     return {'time': time.time()}
+
+@user_api.route('/create_user', methods=['POST'])
+def create_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('hashed_password')
+    cur = mysql.connection.cursor()
+    try:
+        cur.execute(
+            "INSERT INTO usertest(username, password) VALUES (%s, %s)", (username, password))
+    except Exception as e:
+        return str(e)
+    finally:
+        mysql.connection.commit()
+        cur.close()
+
+    return {'time': username, 'card': password}
