d8846b5c0505f4ecf5115fe2a06d3d8c902fea23
JunHao <junhao12345@hotmail.com>
2022-04-10 21:44:07 +0800

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 
 import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/LoginComponent';
+import Register from './pages/RegisterComponent';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
