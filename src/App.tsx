efa0027fbe7d0187958bb4b0c14366e7f187a03a
zhi yao <teezhiyao@gmail.com>
2022-05-17 04:10:55 +0800

    Update login and register component styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,49 @@ import Home from './component/HomeComponent';
 import Login from './pages/LoginComponent';
 import Register from './pages/RegisterComponent';
 import NavBarComponent from "./component/NavBarComponent";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    containerLogin: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: 400,
+      margin: `${theme.spacing(0)} auto`
+    },
+    loginBtn: {
+      marginTop: theme.spacing(2),
+      flexGrow: 1
+    },
+    
+    headerLogin: {
+      textAlign: 'center',
+      background: '#212121',
+      color: '#fff'
+    },
+    cardLogin: {
+      marginTop: theme.spacing(10)
+    }
+  })
+);
 
 
 function App() {
   return (
+    <div>
+    <NavBarComponent></NavBarComponent>
+
     <BrowserRouter>
-      <NavBarComponent></NavBarComponent>
 
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="home" element={<Home/>} />
-        <Route path="deck" element={<Deck/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/deck" element={<Deck/>} />
       </Routes>
-    </BrowserRouter>);
+    </BrowserRouter>
+    </div>
+    );
 }
 
 export default App;
