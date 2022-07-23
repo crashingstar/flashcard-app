83c4e09ef4c64c190cbcc59dc1cf1e7f2997bc24
Tee Zhi Yao <teezhiyao@gmail.com>
2022-07-24 02:47:50 +0800

    Remove Deck from navigation bar

--- src/component/NavBarComponent.tsx
+++ src/component/NavBarComponent.tsx
@@ -1,55 +1,56 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
 
 const pages = [
-  ['Home','/home'], 
-  ['Deck','/deck'], 
-  ['Settings','/settingsTest'], 
-  ['Logout','/logout']];
+  ["Home", "/home"],
+  ["Settings", "/settings"],
+  ["Logout", "/logout"],
+];
 
 const rightNavLinks = [
-    ['Login','/login'], 
-    ['Register','/register']
-  ];
-
+  ["Login", "/login"],
+  ["Register", "/register"],
+];
 
 const NavBarComponent = () => {
-
   return (
     <div>
-    <AppBar position="static" >
+      <AppBar position="static">
         <Toolbar>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page[0]}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
                 href={page[1]}
-                >
+              >
                 {page[0]}
               </Button>
             ))}
           </Box>
-          <Box sx={{ 
-            right: '0%',
-            position: 'absolute',
-            display: { xs: 'none', md: 'flex' } 
-            }}> {rightNavLinks.map((navigationLink) => (
-              <Button  
+          <Box
+            sx={{
+              right: "0%",
+              position: "absolute",
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            {" "}
+            {rightNavLinks.map((navigationLink) => (
+              <Button
                 key={navigationLink[0]}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
                 href={navigationLink[1]}
-                >
+              >
                 {navigationLink[0]}
               </Button>
-              ))
-            }
+            ))}
           </Box>
         </Toolbar>
-    </AppBar>
+      </AppBar>
     </div>
   );
 };
