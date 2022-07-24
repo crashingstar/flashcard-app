1bd727cfeff8ac63378b78489b3ec43f20ce4ccd
Tee Zhi Yao <teezhiyao@gmail.com>
2022-07-25 02:23:33 +0800

    Update Deck component UI

--- src/component/shared/Deck.tsx
+++ src/component/shared/Deck.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
-// import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 export default interface DeckType {
   deck_name: string;
@@ -37,11 +49,20 @@ export function createDeckData(
 export const Deck: React.FC<DeckType> = (props) => {
   return (
     <div>
-      <h2>Deck Name: {props.deck_name}</h2>
-      <h2>Total Flash card: {props.total_cards}</h2>
-      <h2>Date created: {props.date_created}</h2>
-      <h2>Last accessed: {props.last_accessed}</h2>
-      <h2>Deck Id: {props.deck_id}</h2>
+      <List sx={{ maxWidth: 360 }}>
+        <ListItem>
+          <ListItemText primary="Deck Name: " />
+          <ListItemText primary={props.deck_name} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Total Flash card: " />
+          <ListItemText primary={props.total_cards} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Date created: " />
+          <ListItemText primary={props.date_created} />
+        </ListItem>
+      </List>
       <Button variant="contained" size="large">
         <Link color="inherit" href={`/deck/review/${props.deck_id}`}>
           Review
@@ -52,7 +73,39 @@ export const Deck: React.FC<DeckType> = (props) => {
       </Button>
       <Button variant="contained" size="large">
         <Link color="inherit">Delete</Link>
-      </Button>
+      </Button>{" "}
+      {/* <Card sx={{ maxWidth: 360 }}>
+        <CardContent>
+          <List>
+            <ListItem>
+              <ListItemText primary="Deck Name: " />
+              <ListItemText primary={props.deck_name} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Total Flash card: " />
+              <ListItemText primary={props.total_cards} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Date created: " />
+              <ListItemText primary={props.date_created} />
+            </ListItem>
+          </List>
+        </CardContent>
+
+        <CardActions>
+          <Button variant="contained" size="large">
+            <Link color="inherit" href={`/deck/review/${props.deck_id}`}>
+              Review
+            </Link>
+          </Button>
+          <Button variant="contained" size="large">
+            <Link color="inherit">Edit</Link>
+          </Button>
+          <Button variant="contained" size="large">
+            <Link color="inherit">Delete</Link>
+          </Button>{" "}
+        </CardActions>
+      </Card> */}
     </div>
   );
 };
